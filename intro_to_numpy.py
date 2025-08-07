a88c543905846504c330b1767290cba33f94d5e7
tausifs-git <tausifrahman2001@gmail.com>
2025-08-07 23:00:03 +0600

    Add ipynb file

---
+++ intro_to_numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+print(np.__version__)       # 2.2.4 - prev version
+                            # 2.3.2 - after upgrade 
+                            
+                            
+
